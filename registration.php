b51fdd7154c0d36eaf5900f3093bc641021b6489
Mario <mario@nordcomputer.de>
2021-03-19 14:34:47 +0100

    [refactoring] magento-coding-standard

--- registration.php
+++ registration.php
@@ -1,6 +1,6 @@
 <?php
 \Magento\Framework\Component\ComponentRegistrar::register(
-	\Magento\Framework\Component\ComponentRegistrar::MODULE,
-	'Nordcomputer_Showoutofstockprice',
-	__DIR__
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Nordcomputer_Showoutofstockprice',
+    __DIR__
 );
